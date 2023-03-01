f699334ca5b75dcf3f71c9706d765134a30a4093
EuniceNam <caellime@gmail.com>
2023-03-02 08:47:36 +0900

    Initial Commit

---
+++ ConcurrencyStudyImageDownloadUITests/ConcurrencyStudyImageDownloadUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ConcurrencyStudyImageDownloadUITestsLaunchTests.swift
+//  ConcurrencyStudyImageDownloadUITests
+//
+//  Created by Hyorim Nam on 2023/03/02.
+//
+
+import XCTest
+
+final class ConcurrencyStudyImageDownloadUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
